98eb836bccecb1137dd30306da34d2f2e6c37ebb
parksb <parkgds@gmail.com>
2023-09-26 22:48:23 +0900

    Write simple client layout

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>The Work of Art in the Age of Mechanical Generation</title>
+</svelte:head>
+
+<div class="root">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Art-portrait-collage_2.jpg/600px-Art-portrait-collage_2.jpg" />
+  <div class="label">
+    <p class="title">No. 01</p>
+    <p class="material">2023, Oil on canvas</p>
+    <p class="size">13cm x 13cm</p>
+  </div>
+</div>
+
+<style>
+  .root, .label {
+    display: flex;
+  }
+
+  .root {
+    justify-content: center;
+  }
+
+  .label {
+    flex-direction: column;
+    justify-content: flex-end;
+    margin-left: 20px;
+    color: white;
+
+    & > p {
+      margin: 0;
+    }
+
+    & > p.title {
+      font-size: 1.3rem;
+      font-weight: bold;
+    }
+  }
+</style>
