34575b08a86bf72b036421f33f3f3d49c0446b14
parksb <parkgds@gmail.com>
2023-09-27 11:13:31 +0900

    Add audio

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,3 +1,19 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    const audio = new Audio('/sfx.mp3');
+    audio.loop = true;
+    audio.play();
+
+    const synth = window.speechSynthesis;
+    const greeting = 'Hello, welcome to an exhibition that explores art in a whole new dimension. Here, we delve into the fascinating world of Artificial Intelligence and its role in creating artwork. We have two AI models. The first model is the one that speaks about the artwork it intends to create. The second model is the one that listens to the description and translates it into an actual visual piece. These two models work closely in collaboration to continuously produce artworks. We are moving beyond an era of mechanical replication to an era of mechanical generation.';
+    const utterance = new SpeechSynthesisUtterance(greeting);
+    utterance.rate = 0.9;
+    synth.speak(utterance);
+  });
+</script>
+
 <svelte:head>
   <title>The Work of Art in the Age of Mechanical Generation</title>
 </svelte:head>
@@ -7,7 +23,7 @@
   <div class="label">
     <p class="title">No. 01</p>
     <p class="material">2023, Oil on canvas</p>
-    <p class="size">13cm x 13cm</p>
+    <p class="size">20 × 20 cm</p>
   </div>
 </div>
 
@@ -25,6 +41,7 @@
     justify-content: flex-end;
     margin-left: 20px;
     color: white;
+    font-family: serif;
 
     & > p {
       margin: 0;
