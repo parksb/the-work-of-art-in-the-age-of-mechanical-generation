6190b3d455c8778395e360279f7133ea92d6f10f
parksb <parkgds@gmail.com>
2023-11-16 14:53:12 +0900

    Update server api

--- server/main.py
+++ server/main.py
@@ -1,11 +1,20 @@
 from io import BytesIO
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 from fastapi.responses import StreamingResponse
 from transformers import pipeline
 import torch
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 llm_pipe = pipeline("text-generation", model="Gustavosta/MagicPrompt-Stable-Diffusion")
 
@@ -23,14 +32,48 @@ def get_version():
     return {"version": "0.1"}
 
 
-# e.g., "Landscape of "
-@app.get("/chat")
-def chat(prompt: str):
+@app.get("/test/chat")
+def test_chat(prompt: str):
     return llm_pipe(prompt)
 
 
-@app.get("/image")
-def get_image(prompt: str):
+@app.get("/test/image")
+def test_image(prompt: str):
     bytes_io = BytesIO()
     df_pipe(prompt).images[0].save(bytes_io, format="PNG")
     return StreamingResponse(BytesIO(bytes_io.getvalue()), media_type="image/png")
+
+
+@app.get("/prompt")
+def prompt(x: str):
+    return llm_pipe(x)[0]["generated_text"]
+
+
+@app.get("/new")
+def new(prompt: str):
+    df_pipe(prompt).images[0].save("generated_image.png")
+    incr_version()
+    return True
+
+
+@app.get("/image")
+def image():
+    return StreamingResponse(open("generated_image.png", "rb"), media_type="image/png")
+
+
+@app.get("/image_ready")
+def image_ready(x: int):
+    f = open("generated_version", "r")
+    version = int(f.readline())
+    if version > x:
+        return True
+    return False
+
+
+def incr_version():
+    f = open("generated_version", "r")
+    version = int(f.readline())
+    f = open("generated_version", "w")
+    f.write(str(version + 1))
+    f.close()
+    return version + 1
